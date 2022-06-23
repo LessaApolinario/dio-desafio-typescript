9ed79c4e432e036cdedcab486e25ad8213134819
LessaApolinario <lessaapolinario@gmail.com>
2022-06-23 11:19:34 -0300

    Add new elements onto the main page

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
-let buttonTeste = document.getElementById('button') as HTMLButtonElement
+const nav = document.createElement('nav')
+document.body.appendChild(nav)
 
-buttonTeste?.addEventListener('click', () => {
-  console.log('funcionou')
-})
+const ul = document.createElement('ul') as HTMLUListElement
+nav.appendChild(ul)
+
+const listItems = [
+  '<li><a href="./html/desafio1.html">Desafio 1</a></li>',
+  '<li><a href="./html/desafio2.html">Desafio 2</a></li>',
+  '<li><a href="./html/desafio3.html">Desafio 3</a></li>',
+  '<li><a href="./html/desafio4.html">Desafio 4</a></li>'
+]
+
+ul.innerHTML = listItems.join('')
