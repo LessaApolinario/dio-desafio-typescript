cf438e0d4f6c672149082b0e7b841cfbeddb1d4d
LessaApolinario <lessaapolinario@gmail.com>
2022-06-23 08:23:45 -0300

    Add answer for exercise 2

---
+++ src/desafios/desafio2.ts
@@ -0,0 +1,48 @@
+// Como podemos melhorar o esse código usando TS?
+
+// eslint-disable-next-line
+enum IProfissao {
+  // eslint-disable-next-line
+  Atriz,
+  // eslint-disable-next-line
+  Padeiro
+}
+
+interface IPessoa {
+  nome: string
+  idade: number
+  profissao: IProfissao
+}
+
+const pessoa1: IPessoa = {
+  nome: 'Maria',
+  idade: 29,
+  profissao: IProfissao.Atriz
+}
+
+const pessoa2: IPessoa = {
+  nome: 'Roberto',
+  idade: 19,
+  profissao: IProfissao.Padeiro
+}
+
+const pessoa3: IPessoa = {
+  nome: 'Laura',
+  idade: 32,
+  profissao: IProfissao.Atriz
+}
+
+const pessoa4: IPessoa = {
+  nome: 'Carlos',
+  idade: 19,
+  profissao: IProfissao.Padeiro
+}
+
+const pessoas: IPessoa[] = []
+
+pessoas.push(pessoa1)
+pessoas.push(pessoa2)
+pessoas.push(pessoa3)
+pessoas.push(pessoa4)
+
+export { pessoas }
